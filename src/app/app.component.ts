7d453a00b19494db8a0dbd660d4af9ff02e98e83
Junaid bin Abdul Razzaq <junaid.arq@gmail.com>
2025-01-21 21:15:48 +0400

    footer implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layouts/full/vertical/footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet,
+      FooterComponent
+    ],
     templateUrl: './app.component.html'
 })
 export class AppComponent {
--- src/app/clients/client-list/client-list.component.ts
+++ src/app/clients/client-list/client-list.component.ts
@@ -257,6 +257,9 @@ export class ClientListComponent implements OnInit, AfterViewInit {
   redirectToAdd() {
     this.router.navigate(['/clients/add']);
   }
+  redirectToFinancial() {
+    this.router.navigate(['/finances']);
+  }
 
   // tslint:disable-next-line - Disables all
   // updateRowData(row_obj: Employee): boolean | any {
