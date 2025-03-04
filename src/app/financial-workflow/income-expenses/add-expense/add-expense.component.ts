59dd742cab87236185b7fda70a7dbcf8a41a5a89
gliterpoint <rehmankabir786@gmail.com>
2025-03-04 17:56:21 +0500

    Added Expense and income Modals

--- src/app/financial-workflow/income-expenses/add-expense/add-expense.component.ts
+++ src/app/financial-workflow/income-expenses/add-expense/add-expense.component.ts
@@ -1,20 +1,44 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-add-expense',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatSliderModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './add-expense.component.html',
-  styleUrl: './add-expense.component.scss'
+  styleUrl: './add-expense.component.scss',
 })
 export class AddExpenseComponent {
-
   constructor(
-      private dialogRef: MatDialogRef<AddExpenseComponent>,
-      @Inject(MAT_DIALOG_DATA) public data: any
-    ) {}
-  
-    closeDialog(): void {
-      this.dialogRef.close();
-    }
+    private dialogRef: MatDialogRef<AddExpenseComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/financial-workflow/income-expenses/add-income/add-income.component.ts
+++ src/app/financial-workflow/income-expenses/add-income/add-income.component.ts
@@ -1,20 +1,44 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-add-income',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatSliderModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './add-income.component.html',
-  styleUrl: './add-income.component.scss'
+  styleUrl: './add-income.component.scss',
 })
 export class AddIncomeComponent {
-
   constructor(
-      private dialogRef: MatDialogRef<AddIncomeComponent>,
-      @Inject(MAT_DIALOG_DATA) public data: any
-    ) {}
-  
-    closeDialog(): void {
-      this.dialogRef.close();
-    }
+    private dialogRef: MatDialogRef<AddIncomeComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
 }
