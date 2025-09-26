982556193820cfd96735107aec4e09afe57836d0
miqbalmanzoor <miqbalmanzoor@outlook.com>
2025-09-27 00:57:53 +0500

    UI-change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,13 +19,13 @@ export const routes: Routes = [
         path: 'clients',
         loadChildren: () =>
           import('./clients/clients.routes').then((m) => m.ClientsRoutes),
-        canActivate: [AuthGuard]
+    //    canActivate: [AuthGuard]
       },
       {
         path: 'cashflows',
         loadChildren: () =>
           import('./financial-workflow/financial-workflow.routes').then((m) => m.FinancialWorkflowRoutes),
-        canActivate: [AuthGuard]
+       // canActivate: [AuthGuard]
       },
       {
         path: 'settings',
@@ -38,7 +38,7 @@ export const routes: Routes = [
           import('./default-preferance/default-preferance.module').then(m => m.DefaultPreferanceModule)
       }
     ],
-    canActivate: [AuthGuard]
+  //  canActivate: [AuthGuard]
   },
   {
     path: '',
