d2687a4165737dbfd6e18ea97d7ed57932740a63
miqbalmanzoor <miqbalmanzoor@outlook.com>
2025-11-15 09:11:55 +0500

    UI-update

--- src/app/financial-workflow/emergencies/emergencies.component.ts
+++ src/app/financial-workflow/emergencies/emergencies.component.ts
@@ -3,9 +3,15 @@ import { combineLatest, map, switchMap, tap } from 'rxjs';
 import { NavItemService } from 'src/app/layouts/full/nav-item.service';
 import { EmergenciesHttpService as EmergenciesHttpService } from './services/emergencies-http.service';
 import { EmergenciesModel as EmergenciesModel } from './models/emergencies.model';
-
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-emergencies',
+   imports: [
+      MatSliderModule,
+  MatCardModule
+    ],
+
   templateUrl: './emergencies.component.html',
   styleUrl: './emergencies.component.scss'
 })
