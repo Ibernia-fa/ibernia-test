67956f2a99b81566f07160d12a2a8ebba9878631
miqbalmanzoor <miqbalmanzoor@outlook.com>
2025-11-24 23:23:45 +0500

    compare model popup

--- src/app/financial-workflow/reports/compare-cashflows/compare-cashflows.component.ts
+++ src/app/financial-workflow/reports/compare-cashflows/compare-cashflows.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-compare-cashflows',
-  imports: [],
+  imports: [
+    MatCheckboxModule,
+  ],
   templateUrl: './compare-cashflows.component.html',
   styleUrl: './compare-cashflows.component.scss'
 })
