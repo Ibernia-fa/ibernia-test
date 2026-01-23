ef7f1fd2601848195abc5d51cef4991013cde34d
Muhammad Inam Aslam <inam.aslam@ngbs.co.uk>
2026-01-23 11:17:19 +0500

    upload and save background image functionality

--- src/app/settings/services/organization.profiles.service.ts
+++ src/app/settings/services/organization.profiles.service.ts
@@ -6,12 +6,14 @@ export interface OrganizationProfileDto {
   id?: string | null;        // optional – keep null if you don't have one yet
   userId: string;            // logged-in user id
   profilePhotoUrl: string;   // data URL (base64) or URL string
+  backgroundPhotoUrl: string;
 }
 
 @Injectable({ providedIn: 'root' })
 export class OrganizationProfilesService {
   private readonly baseUrl = '/api/v1';
   private brandingLogoSource = new BehaviorSubject<string | null>(null);
+  private backgroundImageSource = new BehaviorSubject<string | null>(null);
   public brandingLogo$ = this.brandingLogoSource.asObservable();
 
   constructor(private http: HttpClient) {}
@@ -37,5 +39,9 @@ export class OrganizationProfilesService {
 
   setBrandingLogo(newLogoUrl: string) {
     this.brandingLogoSource.next(newLogoUrl); // + '?v=' + new Date().getTime() 
+  }
+
+  setBackgroundImage(newBackgroundImageUrl: string) {
+    this.backgroundImageSource.next(newBackgroundImageUrl);
   }
 }
