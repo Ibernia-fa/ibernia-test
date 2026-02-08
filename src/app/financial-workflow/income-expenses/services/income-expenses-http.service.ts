a466a3d87503dbaa5fe4ad665c706de7828f7367
Shujaat Ali <shujaat.ali012@gmail.com>
2026-02-09 02:06:24 +0400

    Income expense updates

--- src/app/financial-workflow/income-expenses/services/income-expenses-http.service.ts
+++ src/app/financial-workflow/income-expenses/services/income-expenses-http.service.ts
@@ -12,7 +12,7 @@ export class IncomeExpensesHttpService {
   constructor(private httpClient: HttpClient) { }
 
   getAllIncomeExpenses(cashflowId: string) {
-    return this.httpClient.get<IncomeExpense>(`${this.INCOME_EXPENSE_URL_PREFIX}/${cashflowId}/financial`);
+    return this.httpClient.get<IncomeExpense>(`${this.INCOME_EXPENSE_URL_PREFIX}/${cashflowId}/income-expense/financial`);
   }
 
   addIncome(cashflowId: string, income: FinancialViewModel) {
