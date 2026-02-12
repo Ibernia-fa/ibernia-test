a9812ef5f201418e47df3bf608d6450cd16a348e
Muhammad Inam Aslam <inam.aslam@ngbs.co.uk>
2026-02-12 18:46:54 +0500

    fix custom saving pot not editable

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://api.ibernia.it',
-  apiUrl: "http://localhost:5071",
+  apiUrl: "https://localhost:7071",
   authority: 'https://identity.ibernia.it',
   authClientId: 'b2323429-bc5e-42f1-ac7c-9ece5dd49110',
 };
