e7e41e71283172884e8967bb118752c32d914129
Muhammad Kashif Butt <mkashifbutt@snapchat.com>
2026-02-16 12:46:32 +0400

    environment for identity

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://dev-api.ibernia.it',
-  authority: 'https://identity.ibernia.it',
+  authority: 'https://dev-identity.ibernia.it',
   authClientId: 'b2323429-bc5e-42f1-ac7c-9ece5dd49110',
 };
