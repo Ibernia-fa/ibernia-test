04fc8f838c50105c6569adf77c457b166d223c95
Muhammad Inam Aslam <inam.aslam@ngbs.co.uk>
2026-02-17 22:28:46 +0500

    get questions from API, share link with client, show the questions to the client on link page

---
+++ src/app/clients/services/questionnaire-http.service.ts
@@ -0,0 +1,85 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+export interface QuestionModel {
+  id: string;
+  text: string;
+  type: string;
+  order: number;
+  options: string[];
+}
+
+export interface CreateQuestionnaireLinkRequest {
+  clientId: string;
+  advisorId: string;
+  questionIds: string[];
+}
+
+export interface CreateQuestionnaireLinkResponse {
+  token: string;
+  shareableUrl: string;
+  expiryDate: string;
+}
+
+export interface GetQuestionnaireByTokenResponse {
+  clientName: string;
+  advisorName: string;
+  questions: QuestionModel[];
+}
+
+export interface QuestionnaireResponseItem {
+  questionId: string;
+  questionText?: string;
+  type: string;
+  value: unknown;
+}
+
+export interface SubmitQuestionnaireRequest {
+  responses: QuestionnaireResponseItem[];
+}
+
+export interface GetClientQuestionnaireResponse {
+  clientId: string;
+  advisorName: string;
+  submittedAt: string;
+  responses: QuestionnaireResponseItem[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionnaireHttpService {
+  readonly BASE_URL = '/api/v1/Questionnaire';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getQuestions() {
+    return this.httpClient.get<QuestionModel[]>(`${this.BASE_URL}/questions`);
+  }
+
+  createLink(request: CreateQuestionnaireLinkRequest) {
+    return this.httpClient.post<CreateQuestionnaireLinkResponse>(
+      `${this.BASE_URL}/create`,
+      { ...request, questionIds: request.questionIds }
+    );
+  }
+
+  getByToken(token: string) {
+    return this.httpClient.get<GetQuestionnaireByTokenResponse>(
+      `${this.BASE_URL}/view/${token}`
+    );
+  }
+
+  submit(token: string, request: SubmitQuestionnaireRequest) {
+    return this.httpClient.post<{ message: string }>(
+      `${this.BASE_URL}/submit/${token}`,
+      request
+    );
+  }
+
+  getClientResponses(clientId: string) {
+    return this.httpClient.get<GetClientQuestionnaireResponse>(
+      `${this.BASE_URL}/client/${clientId}`
+    );
+  }
+}
