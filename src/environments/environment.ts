892df6657f790d80bc78eacd0034e34a8716f070
Muhammad Inam Aslam <inam.aslam@ngbs.co.uk>
2026-02-24 14:17:39 +0500

    fix profile not showing properly

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:7071',
-  authority: 'https://identity.ibernia.it',
+  authority: 'https://dev-identity.ibernia.it',
   adminUrl: 'https://dev-admin.ibernia.it',
   authClientId: 'b2323429-bc5e-42f1-ac7c-9ece5dd49110',
 };
